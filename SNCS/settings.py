ccf9b76ba813346b0c3660eb438ab296d6caaedb
sassawat <tong_sasawat@hotmail.com>
2018-08-08 11:58:09 +0700

    Add application
    add app website and template

--- SNCS/settings.py
+++ SNCS/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SNCS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['website/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/website/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'website/static',
+]
